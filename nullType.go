97f01fd2246ea8978a26b03260295d5c32c17dfa
toyaha <github.dfja3@gmail.com>
2020-02-03 00:05:37 +0900

    nulltype support database

--- nullType.go
+++ nullType.go
@@ -1,34 +1,34 @@
 package goson
 
 import (
+	"database/sql"
 	"encoding/json"
 	"time"
 )
 
 type NullBool struct {
-	Value bool
-	Valid bool
+	sql.NullBool
 }
 
 func (rec *NullBool) Get() *bool {
-	if !rec.Value {
+	if !rec.NullBool.Valid {
 		return nil
 	}
 
-	val := rec.Value
+	val := rec.NullBool.Bool
 
 	return &val
 }
 
 func (rec *NullBool) Set(value bool) {
-	rec.Value = value
-	rec.Valid = true
+	rec.NullBool.Bool = value
+	rec.NullBool.Valid = true
 }
 
 func (rec *NullBool) Delete() {
 	var val bool
-	rec.Value = val
-	rec.Valid = false
+	rec.NullBool.Bool = val
+	rec.NullBool.Valid = false
 }
 
 func (rec NullBool) MarshalJSON() ([]byte, error) {
@@ -36,7 +36,7 @@ func (rec NullBool) MarshalJSON() ([]byte, error) {
 		return json.Marshal(nil)
 	}
 
-	return json.Marshal(rec.Value)
+	return json.Marshal(rec.Bool)
 }
 
 func (rec *NullBool) UnmarshalJSON(value []byte) error {
@@ -52,36 +52,35 @@ func (rec *NullBool) UnmarshalJSON(value []byte) error {
 		return nil
 	}
 
-	rec.Value = *val
+	rec.Bool = *val
 	rec.Valid = true
 
 	return nil
 }
 
 type NullFloat64 struct {
-	Value float64
-	Valid bool
+	sql.NullFloat64
 }
 
 func (rec *NullFloat64) Get() *float64 {
-	if !rec.Valid {
+	if !rec.NullFloat64.Valid {
 		return nil
 	}
 
-	val := rec.Value
+	val := rec.NullFloat64.Float64
 
 	return &val
 }
 
 func (rec *NullFloat64) Set(value float64) {
-	rec.Value = value
-	rec.Valid = true
+	rec.NullFloat64.Float64 = value
+	rec.NullFloat64.Valid = true
 }
 
 func (rec *NullFloat64) Delete() {
 	var val float64
-	rec.Value = val
-	rec.Valid = false
+	rec.NullFloat64.Float64 = val
+	rec.NullFloat64.Valid = false
 }
 
 func (rec NullFloat64) MarshalJSON() ([]byte, error) {
@@ -89,7 +88,7 @@ func (rec NullFloat64) MarshalJSON() ([]byte, error) {
 		return json.Marshal(nil)
 	}
 
-	return json.Marshal(rec.Value)
+	return json.Marshal(rec.Float64)
 }
 
 func (rec *NullFloat64) UnmarshalJSON(value []byte) error {
@@ -105,36 +104,35 @@ func (rec *NullFloat64) UnmarshalJSON(value []byte) error {
 		return nil
 	}
 
-	rec.Value = *val
+	rec.Float64 = *val
 	rec.Valid = true
 
 	return nil
 }
 
 type NullInt32 struct {
-	Value int32
-	Valid bool
+	sql.NullInt32
 }
 
 func (rec *NullInt32) Get() *int {
-	if !rec.Valid {
+	if !rec.NullInt32.Valid {
 		return nil
 	}
 
-	val := int(rec.Value)
+	val := int(rec.NullInt32.Int32)
 
 	return &val
 }
 
 func (rec *NullInt32) Set(value int) {
-	rec.Value = int32(value)
-	rec.Valid = true
+	rec.NullInt32.Int32 = int32(value)
+	rec.NullInt32.Valid = true
 }
 
 func (rec *NullInt32) Delete() {
-	var val int
-	rec.Value = int32(val)
-	rec.Valid = false
+	var val int32
+	rec.NullInt32.Int32 = val
+	rec.NullInt32.Valid = false
 }
 
 func (rec NullInt32) MarshalJSON() ([]byte, error) {
@@ -142,11 +140,11 @@ func (rec NullInt32) MarshalJSON() ([]byte, error) {
 		return json.Marshal(nil)
 	}
 
-	return json.Marshal(rec.Value)
+	return json.Marshal(rec.Int32)
 }
 
 func (rec *NullInt32) UnmarshalJSON(value []byte) error {
-	var val *int
+	var val *int32
 
 	err := json.Unmarshal(value, &val)
 	if err != nil {
@@ -158,36 +156,35 @@ func (rec *NullInt32) UnmarshalJSON(value []byte) error {
 		return nil
 	}
 
-	rec.Value = int32(*val)
+	rec.Int32 = *val
 	rec.Valid = true
 
 	return nil
 }
 
 type NullInt64 struct {
-	Value int64
-	Valid bool
+	sql.NullInt64
 }
 
 func (rec *NullInt64) Get() *int {
-	if !rec.Valid {
+	if !rec.NullInt64.Valid {
 		return nil
 	}
 
-	val := int(rec.Value)
+	val := int(rec.NullInt64.Int64)
 
 	return &val
 }
 
 func (rec *NullInt64) Set(value int) {
-	rec.Value = int64(value)
-	rec.Valid = true
+	rec.NullInt64.Int64 = int64(value)
+	rec.NullInt64.Valid = true
 }
 
 func (rec *NullInt64) Delete() {
-	var val int
-	rec.Value = int64(val)
-	rec.Valid = false
+	var val int64
+	rec.NullInt64.Int64 = val
+	rec.NullInt64.Valid = false
 }
 
 func (rec NullInt64) MarshalJSON() ([]byte, error) {
@@ -195,11 +192,11 @@ func (rec NullInt64) MarshalJSON() ([]byte, error) {
 		return json.Marshal(nil)
 	}
 
-	return json.Marshal(rec.Value)
+	return json.Marshal(rec.Int64)
 }
 
 func (rec *NullInt64) UnmarshalJSON(value []byte) error {
-	var val *int
+	var val *int64
 
 	err := json.Unmarshal(value, &val)
 	if err != nil {
@@ -211,36 +208,35 @@ func (rec *NullInt64) UnmarshalJSON(value []byte) error {
 		return nil
 	}
 
-	rec.Value = int64(*val)
+	rec.Int64 = *val
 	rec.Valid = true
 
 	return nil
 }
 
 type NullString struct {
-	Value string
-	Valid bool
+	sql.NullString
 }
 
 func (rec *NullString) Get() *string {
-	if !rec.Valid {
+	if !rec.NullString.Valid {
 		return nil
 	}
 
-	val := rec.Value
+	val := rec.NullString.String
 
 	return &val
 }
 
 func (rec *NullString) Set(value string) {
-	rec.Value = value
-	rec.Valid = true
+	rec.NullString.String = value
+	rec.NullString.Valid = true
 }
 
 func (rec *NullString) Delete() {
 	var val string
-	rec.Value = val
-	rec.Valid = false
+	rec.NullString.String = val
+	rec.NullString.Valid = false
 }
 
 func (rec NullString) MarshalJSON() ([]byte, error) {
@@ -248,7 +244,7 @@ func (rec NullString) MarshalJSON() ([]byte, error) {
 		return json.Marshal(nil)
 	}
 
-	return json.Marshal(rec.Value)
+	return json.Marshal(rec.String)
 }
 
 func (rec *NullString) UnmarshalJSON(value []byte) error {
@@ -264,36 +260,35 @@ func (rec *NullString) UnmarshalJSON(value []byte) error {
 		return nil
 	}
 
-	rec.Value = *val
+	rec.String = *val
 	rec.Valid = true
 
 	return nil
 }
 
 type NullTime struct {
-	Value time.Time
-	Valid bool
+	sql.NullTime
 }
 
 func (rec *NullTime) Get() *time.Time {
-	if !rec.Valid {
+	if !rec.NullTime.Valid {
 		return nil
 	}
 
-	val := rec.Value
+	val := rec.NullTime.Time
 
 	return &val
 }
 
 func (rec *NullTime) Set(value time.Time) {
-	rec.Value = value
-	rec.Valid = true
+	rec.NullTime.Time = value
+	rec.NullTime.Valid = true
 }
 
 func (rec *NullTime) Delete() {
 	var val time.Time
-	rec.Value = val
-	rec.Valid = false
+	rec.NullTime.Time = val
+	rec.NullTime.Valid = false
 }
 
 func (rec NullTime) MarshalJSON() ([]byte, error) {
@@ -301,7 +296,7 @@ func (rec NullTime) MarshalJSON() ([]byte, error) {
 		return json.Marshal(nil)
 	}
 
-	return json.Marshal(rec.Value)
+	return json.Marshal(rec.Time)
 }
 
 func (rec *NullTime) UnmarshalJSON(value []byte) error {
@@ -317,7 +312,7 @@ func (rec *NullTime) UnmarshalJSON(value []byte) error {
 		return nil
 	}
 
-	rec.Value = *val
+	rec.Time = *val
 	rec.Valid = true
 
 	return nil
